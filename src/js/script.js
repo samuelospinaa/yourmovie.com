dda8909c424ef4a0d91eca66ce12594db2c51c7f
Samuel <samuelospina443@gmail.com>
2024-12-18 18:08:04 -0500

    Adding new function that call an API for streaming avaibility

--- src/js/script.js
+++ src/js/script.js
@@ -1,70 +1,109 @@
 const $ = (selector) => document.querySelector(selector);
 
-const apiKEY = "0f1e85c7df926b373c71b2470adc50c7";
+const API_KEY = "0f1e85c7df926b373c71b2470adc50c7";
+const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+const DEFAULT_IMAGE = "./public/th.png";
 
-const button = $("#search-button");
-const sectionView = $("#section-movie-view");
-const mainSection = $("#main-section");
-const article = $("article");
-const leftAnimation = $(".lateral-left");
-const rightAnimation = $(".lateral-right");
+const elements = {
+  button: $("#search-button"),
+  sectionView: $("#section-movie-view"),
+  mainSection: $("#main-section"),
+  article: $("article"),
+  leftAnimation: $(".lateral-left"),
+  rightAnimation: $(".lateral-right"),
+};
 
-function createView(data) {
-  const movie = data.results[0];
-  const baseUrl = "https://image.tmdb.org/t/p/w500";
-  const notImageRoute = "./public/th.png";
-  const imageUrl = movie.poster_path
-    ? `${baseUrl}${movie.poster_path}`
-    : notImageRoute;
+const PLATFORM_IMAGES = {
+  Netflix: "../public/netflix.svg",
+  "Amazon Prime": "../public/amazon.svg",
+  "Disney+": "../public/disney.svg",
+  "HBO Max": "../public/hbologo.svg",
+};
 
-  const htmlView = `
-        <img src="${imageUrl} alt="${movie.title}" class="image" style="max-width: 400px; border-radius: 10px; margin-top: 25px;">
-        <h2 class="title">${movie.title}</h2>
-        <p class="overview">${movie.overview}</p>
-        <div class="data">
-        <p><strong>Rating: ${movie.vote_average}</strong><</p>
-        <p><strong>Release date: ${movie.release_date}</strong></p>
-        <button id="other-search">No es lo que buscaba</button>
-        </div>`;
+function getStreamingPlatforms(platforms) {
+  if (!platforms || !platforms.length) return [];
 
-  const divView = document.createElement("div");
-  divView.id = "movie-view";
-
-  divView.innerHTML = htmlView;
-
-  sectionView.appendChild(divView);
-
-  // button that let the user search for another movie
-  const buttonAnother = $("#other-search");
-
-  if (buttonAnother) {
-    // if the user are searching for another thing we send him to the main section
-    buttonAnother.addEventListener("click", () => {
-      window.location.reload();
-    });
-  }
-  mainSection.style.display = "none";
-  leftAnimation.style.display = "none";
-  rightAnimation.style.display = "none";
+  return platforms
+    .filter((platform) => platform.streamingType === "subscription")
+    .map((platform) => ({
+      service: platform.service,
+      image: PLATFORM_IMAGES[platform.service] || DEFAULT_IMAGE,
+    }));
 }
 
-function saveSearch() {
-  //const to recover the value of the search input
-  const search = $("#search").value;
-  //sending the search to the localstorage
-  localStorage.setItem("movieTitle", search);
+function createPlatformsList(platforms) {
+  if (!platforms.length) return "<p>No subscription platforms available</p>";
 
-  //validation for the input search
-  if (!search) {
-    alert("Please enter a movie title");
-    return;
+  return `
+    <h3>Available on:</h3>
+    <ul>
+      ${platforms
+        .map(
+          (platform) =>
+            `<li><img src="${platform.image}" alt="${platform.service}"></li>`
+        )
+        .join("")}
+    </ul>
+  `;
+}
+
+function createView(movieData, streamingData) {
+  const movie = movieData.results[0];
+  const platforms = streamingData
+    ? getStreamingPlatforms(streamingData[0]?.streamingInfo?.us)
+    : [];
+
+  const imageUrl = movie.poster_path
+    ? `${BASE_IMAGE_URL}${movie.poster_path}`
+    : DEFAULT_IMAGE;
+
+  const htmlView = `
+    <div id="movie-view">
+      <img 
+        src="${imageUrl}" 
+        alt="${movie.title}" 
+        class="image" 
+        style="max-width: 400px; border-radius: 10px; margin-top: 25px;"
+      >
+      <h2 class="title">${movie.title}</h2>
+      <p class="overview">${movie.overview}</p>
+      <div class="platforms">
+        ${createPlatformsList(platforms)}
+      </div>
+      <div class="data">
+        <p><strong>Rating: ${movie.vote_average}</strong></p>
+        <p><strong>Release date: ${movie.release_date}</strong></p>
+        <button id="other-search">Search another movie</button>
+      </div>
+    </div>
+  `;
+
+  elements.sectionView.innerHTML = htmlView;
+  setupOtherSearchButton();
+  hideMainElements();
+}
+
+function setupOtherSearchButton() {
+  const buttonAnother = $("#other-search");
+  if (buttonAnother) {
+    buttonAnother.addEventListener("click", () => window.location.reload());
   }
-  // for every time that the user click on the button the fetchmvoie function will be called
-  fetchMovie(search);
+}
+
+function hideMainElements() {
+  elements.mainSection.style.display = "none";
+  elements.leftAnimation.style.display = "none";
+  elements.rightAnimation.style.display = "none";
+}
+
+function validateSearch(search) {
+  if (!search?.trim()) {
+    throw new Error("Please enter a movie title");
+  }
+  return search.trim();
 }
 
 async function fetchMovie(search) {
-  //object options for the fetch
   const options = {
     method: "GET",
     headers: {
@@ -74,24 +113,66 @@ async function fetchMovie(search) {
     },
   };
 
-  // main url to fetch the movie data
-  const url = `https://api.themoviedb.org/3/search/movie?query=${search}&api_key=${apiKEY}`;
+  const response = await fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
+      search
+    )}&api_key=${API_KEY}`,
+    options
+  );
 
-  const response = await fetch(url, options);
-  if (!response.ok) throw new Error("Failed to fetch movie data");
+  if (!response.ok) {
+    throw new Error(`Failed to fetch movie data: ${response.statusText}`);
+  }
 
-  //allowing the response in a const to be used
   const data = await response.json();
 
-  //validation if the movie that the user is searching is not found
-  if (!data.results || data.results.length === 0) {
-    alert("Movie not found");
+  if (!data.results?.length) {
+    throw new Error("Movie not found");
   }
-  // if the movie is found the createView function will be called
-  if (data) {
-    createView(data);
+
+  return data;
+}
+
+async function fetchPlatforms(search) {
+  const options = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "139c5deaa1msh9968c6bf54ddb59p1c1999jsn5da2620328ea",
+      "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
+    },
+  };
+
+  const response = await fetch(
+    `https://streaming-availability.p.rapidapi.com/search/title?title=${encodeURIComponent(
+      search
+    )}&country=us&show_type=movie&output_language=en`,
+    options
+  );
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch platform data: ${response.statusText}`);
+  }
+
+  return response.json();
+}
+
+async function handleSearch() {
+  try {
+    const searchInput = $("#search");
+    const search = validateSearch(searchInput.value);
+
+    localStorage.setItem("movieTitle", search);
+
+    const [movieData, platformData] = await Promise.all([
+      fetchMovie(search),
+      fetchPlatforms(search),
+    ]);
+
+    createView(movieData, platformData);
+  } catch (error) {
+    alert(error.message);
+    console.error("Search error:", error);
   }
 }
 
-//adding an event listener to the button for save the user search
-button.addEventListener("click", saveSearch);
+elements.button.addEventListener("click", handleSearch);
